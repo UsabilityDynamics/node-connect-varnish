df8cd6102bf031829597039935b5b2f3b6fbf60a
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-03-13 11:10:41 -0400

    dep modification

--- test/api.js
+++ test/api.js
@@ -3,3 +3,24 @@
  *
  *
  */
+module.exports = {
+
+  'Connect Varnish': {
+
+    'returns expected methods': function () {
+
+      var cVarnish = require( '../' );
+
+      cVarnish.should.have.property( 'debug' );
+      cVarnish.should.have.property( 'create' );
+      cVarnish.should.have.property( 'version' );
+      cVarnish.should.have.property( 'defaults' );
+      cVarnish.should.have.property( 'prototype' );
+      cVarnish.prototype.should.have.property( 'response' );
+      cVarnish.prototype.should.have.property( 'utility' );
+
+    }
+
+  }
+
+}
